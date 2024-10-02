20732acf1bae2da55a548b367f98eea842302158
Johannes Henz <johenz@protonmail.com>
2024-10-02 16:51:59 +0200

    Made a Basic Spring structure with mocked db

---
+++ SWEN3-Document-Management-Backend/src/main/java/Group_C/SWEN3_Document_Management_Backend/Swen3DocumentManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package Group_C.SWEN3_Document_Management_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swen3DocumentManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swen3DocumentManagementBackendApplication.class, args);
+	}
+
+}
---
+++ SWEN3-Document-Management-Backend/src/test/java/Group_C/SWEN3_Document_Management_Backend/Swen3DocumentManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Group_C.SWEN3_Document_Management_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swen3DocumentManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
