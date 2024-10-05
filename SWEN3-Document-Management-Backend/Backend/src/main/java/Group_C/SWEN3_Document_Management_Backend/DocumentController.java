60be7a93deef8511ac66531eec3ed79098f55294
JohannesHenz <if21b136@technikum-wien.at>
2024-10-05 12:47:01 +0200

    Sprint 1 erfüllt

--- SWEN3-Document-Management-Backend/Backend/src/main/java/Group_C/SWEN3_Document_Management_Backend/DocumentController.java
+++ SWEN3-Document-Management-Backend/Backend/src/main/java/Group_C/SWEN3_Document_Management_Backend/DocumentController.java
@@ -1,4 +1,16 @@
 package Group_C.SWEN3_Document_Management_Backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
 public class DocumentController {
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello, this is your Document Management System.";
+    }
 }
+
